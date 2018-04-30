2eca2510520ee099a3b910b2ed6dae98ceb4b812
Fan <ofan@MTVL16092ef85.local>
2018-04-30 09:32:34 -0700

    change to Car.js

---
+++ src/components/Car.js
@@ -0,0 +1,9 @@
+export default class Car {
+    constructor(make, model, year, color, price) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -5,22 +5,11 @@ import './index.css';
 // Without curly brace: No matter what you name the module you import. Always refers to the default import.
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Car from './components/Car'
 
 const carList = [
-    {
-        make: 'Porche',
-        model: '911',
-        year: 2018,
-        color: 'black',
-        price: 100000
-    },
-    {
-        make: 'Benz',
-        model: 'G63',
-        year: 2018,
-        color: 'black',
-        price: 100000
-    }
+    new Car('Porche', '911', 2018, 'black', 100000),
+    new Car('Benz', 'G63', 2018, 'black', 100000)
 ]
 
 ReactDOM.render(
